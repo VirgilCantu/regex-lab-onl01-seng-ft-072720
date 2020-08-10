e094ba19b22b3aed1339d00e30daff51ce0a5ff8
Virgil Cantu <cantu.sampsom@gmail.com>
2020-08-10 18:16:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,16 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+
+def starts_with_a_vowel?(word)
+  word.start_with?(/[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
